590e9574ca719e92906be5ae1eea4adccb2eb10c
ddotx <domemacintosh@gmail.com>
2021-05-01 22:15:02 +0700

    feat: scully

---
+++ scully.play-jamstack-ng-explorer.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "play-jamstack-ng-explorer",
+  outDir: './dist/static',
+  routes: {
+  }
+};
